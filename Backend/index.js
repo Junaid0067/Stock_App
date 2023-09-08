62c76fe44c9c10246265694f6c0ab30a1c30072c
Mohd Junaid <junaid.barkaati@gmail.com>
2023-09-08 18:41:22 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,74 @@
-const express = require('express')
-const app = express()
-const PORT = 8080;
-const connectToMongoDB = require('./db');
-var cors = require('cors');
-const axios = require('axios');
-const  appRoute  = require('./Routes/appRoute');
-connectToMongoDB();
+import React, { useState, useEffect } from 'react';
 
-app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+function StockDropdown() {
+  const [stockData, setStockData] = useState([]);
+  const [selectedStock, setSelectedStock] = useState('');
+  const [selectedPrice, setSelectedPrice] = useState(null);
 
-app.use('/', appRoute);
-// Setting up polling to insert random stock prices for all stocks every minute
-const updateStockPrices = () => {
-    setInterval(async () => {
+  useEffect(() => {
+    // Fetch stock data from your updated API endpoint (adjust the URL)
+    fetch('http://localhost:3001/api/stocks')
+      .then((response) => response.json())
+      .then((data) => {
+        setStockData(data);
+        if (data.length > 0) {
+          // Check if the selectedStock is still present in the fetched data
+          if (data.find((stock) => stock.stockName === selectedStock)) {
+            setSelectedPrice(data[0].price);
+          } else {
+            setSelectedStock(data[0].stockName);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching stock data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Function to fetch and update the price for the selected stock
+    const fetchPriceForSelectedStock = async () => {
       try {
-        await axios.post('https://stock-tracker-d16p.onrender.com/update');
+        const response = await fetch(`http://localhost:3001/api/stocks/${selectedStock}`);
+        const data = await response.json();
+        if (data.price) {
+          setSelectedPrice(data.price);
+        }
       } catch (error) {
-        console.error('Error updating stock prices:', error);
+        console.error('Error fetching stock price:', error);
       }
-    }, 2000); // 1 minute interval
+    };
+
+    // Fetch price initially when the component mounts
+    fetchPriceForSelectedStock();
+
+    // Set up an interval to fetch updated price every minute
+    const interval = setInterval(fetchPriceForSelectedStock, 60000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, [selectedStock]);
+
+  const handleDropdownChange = (event) => {
+    const selectedStockName = event.target.value;
+    setSelectedStock(selectedStockName);
   };
-  
 
+  return (
+    <div>
+      <h2>Select a Stock:</h2>
+      <select value={selectedStock} onChange={handleDropdownChange}>
+        {stockData.map((stock) => (
+          <option key={stock._id} value={stock.stockName}>
+            {stock.stockName}
+          </option>
+        ))}
+      </select>
+      {selectedPrice !== null && (
+        <p>Selected Price for {selectedStock}: ${selectedPrice}</p>
+      )}
+    </div>
+  );
+}
 
-app.listen(PORT, function(err){
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", PORT);
-    updateStockPrices();
-})
+export default StockDropdown;
