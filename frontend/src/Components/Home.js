84cffb17c902493d10ed73abe5bf38609de5ca8d
Mohd Junaid <junaid.barkaati@gmail.com>
2023-09-08 18:46:01 +0530

    Update Home.js

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -5,46 +5,53 @@ function StockDropdown() {
   const [selectedStock, setSelectedStock] = useState('');
   const [selectedPrice, setSelectedPrice] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://stock-tracker-d16p.onrender.com/getStocksData');
-      const data = await response.json();
-      setStockData(data);
-
-      // Check if the selected stock is still present in the fetched data
-      const selectedStockData = data.find((stock) => stock.stockName === selectedStock);
-      if (selectedStockData) {
-        setSelectedPrice(selectedStockData.price);
-      } else {
-        // If selected stock is not present, select the first stock
-        setSelectedStock(data.length > 0 ? data[0].stockName : '');
-      }
-    } catch (error) {
-      console.error('Error fetching stock data:', error);
-    }
-  };
+  useEffect(() => {
+    // Fetch stock data from your updated API endpoint (adjust the URL)
+    fetch('https://stock-tracker-d16p.onrender.com/getStocksData')
+      .then((response) => response.json())
+      .then((data) => {
+        setStockData(data);
+        if (data.length > 0) {
+          // Check if the selectedStock is still present in the fetched data
+          if (data.find((stock) => stock.stockName === selectedStock)) {
+            setSelectedPrice(data[0].price);
+          } else {
+            setSelectedStock(data[0].stockName);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching stock data:', error);
+      });
+  }, []);
 
   useEffect(() => {
-    fetchData(); // Initial data fetch
-
-    const interval = setInterval(() => {
-      fetchData(); // Fetch updated data every minute
-    }, 2000);
-
-    return () => {
-      clearInterval(interval); // Clean up the interval
+    // Function to fetch and update the price for the selected stock
+    const fetchPriceForSelectedStock = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/api/stocks/${selectedStock}`);
+        const data = await response.json();
+        if (data.price) {
+          setSelectedPrice(data.price);
+        }
+      } catch (error) {
+        console.error('Error fetching stock price:', error);
+      }
     };
-  }, []); // Empty dependency array to run only once
+
+    // Fetch price initially when the component mounts
+    fetchPriceForSelectedStock();
+
+    // Set up an interval to fetch updated price every minute
+    const interval = setInterval(fetchPriceForSelectedStock, 60000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, [selectedStock]);
 
   const handleDropdownChange = (event) => {
     const selectedStockName = event.target.value;
     setSelectedStock(selectedStockName);
-
-    // Find the corresponding price based on the selected stockName
-    const selectedStockData = stockData.find((stock) => stock.stockName === selectedStockName);
-    if (selectedStockData) {
-      setSelectedPrice(selectedStockData.price);
-    }
   };
 
   return (
